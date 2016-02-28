9d3500a6f859d5bde2ae692b552381221c63a29a
Ryker Schwartzenberger <rykerls@uw.edu>
2016-02-27 16:46:21 -0800

    I'm stuck. Help me!

--- scripts/data-retrieval.r
+++ scripts/data-retrieval.r
@@ -15,8 +15,21 @@ options(stringsAsFactors = FALSE)
 
 base_url <- 'https://api.propublica.org/campaign-finance/v1/'
 cycle <- "2016"
-candidate_id <- "S4VT00033"
+candidate_ids <- c("P60007168","P00003392","P80001571","P60006111")
 
-query <- paste0(base_url, '/', cycle, '/', 'candidates', '/', candidate_id)
-response <- GET(query, add_headers("x-api-key"= " 6PV7DzVbl5ji6l3hAYPh3ElWARo5E9I78KMvfTJi"))
-data <- fromJSON(content(response, type='text', encoding = 'UTF-8'))
+# We need to call getCandidateData for each element in candidate_ids, how?
+getCandidateData(candidate_ids[1])
+
+# Takes an FEC ID in, polls Propublica API for current data and writes 
+# to a .csv file in the data/ directory.
+getCandidateData <- function(id) {
+
+  query <- paste0(base_url, cycle, '/', 'candidates', '/', candidate_id, ".json")
+  response <- GET(query, add_headers("X-API-Key" = "6PV7DzVbl5ji6l3hAYPh3ElWARo5E9I78KMvfTJi"))
+  data <- as.data.frame(fromJSON(content(response, type='text', encoding = 'UTF-8')))
+  df <- data.frame(lapply(data, as.character), stringsAsFactors = FALSE)
+  
+  file_addr <- paste0('data/', Sys.Date(), '_', id, '.csv')
+  write.csv(df, file_addr)
+  
+}
