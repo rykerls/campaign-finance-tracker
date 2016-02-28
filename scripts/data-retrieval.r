6eb5a6c4f388636390180989c969e341ad9ee0e7
Ryker Schwartzenberger <rykerls@uw.edu>
2016-02-27 21:27:46 -0800

    Incorporated lb7's fix into master branch.

--- scripts/data-retrieval.r
+++ scripts/data-retrieval.r
@@ -6,30 +6,36 @@
 # INFO-498F
 # Final Project
 ########################
+# Run this file to update presidential poll data from Propublica API.
 
 library(jsonlite)
-library(dplyr)
 library(httr)
 
 options(stringsAsFactors = FALSE)
 
 base_url <- 'https://api.propublica.org/campaign-finance/v1/'
 cycle <- "2016"
-candidate_ids <- c("P60007168","P00003392","P80001571","P60006111")
 
-# We need to call getCandidateData for each element in candidate_ids, how?
-getCandidateData(candidate_ids[1])
+# FEC ID's for some of the current candidates. Add to this list to begin
+# collecting data on a new candidate.
+candidate_ids <- c("P60007168",
+                   "P00003392",
+                   "P80001571",
+                   "P60006111")
 
 # Takes an FEC ID in, polls Propublica API for current data and writes 
 # to a .csv file in the data/ directory.
 getCandidateData <- function(id) {
-
-  query <- paste0(base_url, cycle, '/', 'candidates', '/', candidate_id, ".json")
+  
+  query <- paste0(base_url, cycle, '/', 'candidates', '/', id, ".json")
   response <- GET(query, add_headers("X-API-Key" = "6PV7DzVbl5ji6l3hAYPh3ElWARo5E9I78KMvfTJi"))
   data <- as.data.frame(fromJSON(content(response, type='text', encoding = 'UTF-8')))
   df <- data.frame(lapply(data, as.character), stringsAsFactors = FALSE)
   
-  file_addr <- paste0('data/', Sys.Date(), '_', id, '.csv')
+  file_addr <- paste0('../data/', Sys.Date(), '_', id, '.csv')
   write.csv(df, file_addr)
   
 }
+
+lapply(candidate_ids, getCandidateData)
+
