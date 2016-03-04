0ae83d833741c45e4e69bb4bf903e7c52cfe2233
Ryker Schwartzenberger <rykerls@uw.edu>
2016-03-03 17:02:32 -0800

    Added code to compile individual candidate information into a single dataframe. Also updated data.

--- scripts/data-retrieval.r
+++ scripts/data-retrieval.r
@@ -7,7 +7,11 @@
 # Final Project
 ########################
 # Run this file to update presidential poll data from Propublica API.
-
+# candidate_data is the resultant dataframe, which is compiled data
+# about the candidates specified in candidate_ids. Source this script
+# in your code and use candidate_data.
+########################
+library(dplyr)
 library(jsonlite)
 library(httr)
 
@@ -28,14 +32,26 @@ candidate_ids <- c("P60007168",
 getCandidateData <- function(id) {
   
   query <- paste0(base_url, cycle, '/', 'candidates', '/', id, ".json")
-  response <- GET(query, add_headers("X-API-Key" = "6PV7DzVbl5ji6l3hAYPh3ElWARo5E9I78KMvfTJi"))
-  data <- as.data.frame(fromJSON(content(response, type='text', encoding = 'UTF-8')))
+  response <- GET(query, add_headers('X-API-Key' = '6PV7DzVbl5ji6l3hAYPh3ElWARo5E9I78KMvfTJi'))
+  data <- as.data.frame(fromJSON(content(response, type='text', encoding = 'UTF-8'))) %>% 
+          flatten()
   df <- data.frame(lapply(data, as.character), stringsAsFactors = FALSE)
   
-  file_addr <- paste0('../data/', Sys.Date(), '_', id, '.csv')
+  file_addr <- paste0('data/', Sys.Date(), '_', id, '.csv')
   write.csv(df, file_addr)
   
 }
 
 lapply(candidate_ids, getCandidateData)
+
+# This will only work if ran on the same date as when the data were
+# queried and saved originally. Copy this code and alter it to read
+# data from a specific date.
+file <- paste0('data/', Sys.Date(), '_', candidate_ids[1], '.csv')
+candidate_data <- read.csv(file)
+for (i in 2:length(candidate_ids)) {
+  file <- paste0('data/', Sys.Date(), '_', candidate_ids[i], '.csv')
+  temp_frame <- read.csv(file)
+  candidate_data <- rbind(candidate_data, temp_frame)
+}
 
